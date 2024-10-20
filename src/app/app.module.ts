cfebfae7ece01b123a343af01219aa34b8dfbe6a
victor <6280636@cegepmontpetit.ca>
2024-10-20 15:18:09 -0400

    FCT Création de Composants
    Création de composats album, artist, show, song aussi configuration de routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
+import { ArtistComponent } from './artist/artist.component';
+import { SongComponent } from './song/song.component';
+import { ShowComponent } from './show/show.component';
+import { provideHttpClient } from '@angular/common/http';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      AlbumComponent,
+      ArtistComponent,
+      SongComponent,
+      ShowComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    GoogleMapsModule,
+    YouTubePlayerModule
+    
+    
+    
+    
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/song/song.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { GoogleService } from '../services/google.service';
+import { SafeResourceUrl } from '@angular/platform-browser';
+import { Album } from '../models/album';
+import { SpotifyService } from '../services/Spotify.service';
+import { Song } from '../models/song';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-song',
+  templateUrl: './song.component.html',
+  styleUrls: ['./song.component.css']
+})
+export class SongComponent implements OnInit {
+album ?: Album;
+tabSongs: Song[] = [];
+  /* @Input()  album!: Album; */
+  /* videoSearchText : string = "";*/
+videoId : string = "";
+  /*videoUrl ?: SafeResourceUrl; */
+
+constructor(public httpService : SpotifyService, public route: ActivatedRoute,
+      public google : GoogleService) { }
+
+ngOnInit() {
+  const albumId = this.route.snapshot.paramMap.get('id');
+  if(albumId){
+  this.loadSongs(albumId)
+  }
+}
+async loadSongs(albumId: string){
+  const album = new Album(albumId, "", "");
+  try {
+      this.tabSongs = await this.httpService.getSongs(album);
+      console.log(this.tabSongs); // Verifica aquí si está recibiendo datos
+        } catch (error) {
+      console.error('Error loading songs:', error);
+        }
+    }
+
+  async serarchVideo(song : Song):Promise<void> {
+    try{
+    this.videoId = await this.google.searchVideoId(song.name);
+    console.log(this.videoId);
+    }catch(error) {
+      console.error('Error searching for video:', error);
+    }
+  }  
+ 
+}
+
